461b8c0bc9d3af8de54166df7d62f2c33f6b3327
Patrickmoussallem <patrick.moussallem@net.usj.edu.lb>
2023-10-26 22:24:37 +0300

    footeradded

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Button } from '../Button';
 import '../../App.css'
 import HeroSection from '../HeroSection';
+import Footer from '../Footer'
 
 export default function Home(){
     return(
         <>
         <HeroSection/>
+        <Footer />
         
         
         
