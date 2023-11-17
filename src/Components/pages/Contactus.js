37f691ddc9f54bb28f2e9c6feb9bf90576ddcf7f
patrickmoussallem <patrick.moussallem@net.usj.edu.lb>
2023-11-17 14:10:50 +0200

    about_page_responsive

--- src/Components/pages/Contactus.js
+++ src/Components/pages/Contactus.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import ContactForm from '../ContactForm';
+import './Contactus.css'
 
 export default function Contactus(){
     return(
