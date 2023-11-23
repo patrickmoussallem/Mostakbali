5639df439fb8abfbfd0a4bee10476abfa7dd494b
Patrickmoussallem <patrick.moussallem@net.usj.edu.lb>
2023-11-24 00:42:27 +0200

    smallfixesincardcomp

--- src/Components/Card2.js
+++ src/Components/Card2.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 
 const UserProfileCard1 = ({ name, webpage,country }) => {
     return (
-      <div className="profile-card">
+      <div className="profile-card1">
         
-        <div className="profile-info">
+        <div className="profile-info1">
           <h4>{name}</h4>
           <Link className='cardlink' underline="hover"  href= '{webpage}'>
           {webpage}
