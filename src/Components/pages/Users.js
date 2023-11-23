934042ffc97c1d772aaf1d28bbc5294110b63b28
patrickmoussallem <patrick.moussallem@net.usj.edu.lb>
2023-11-23 11:30:09 +0200

    userpage_started

--- src/Components/pages/Users.js
+++ src/Components/pages/Users.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from "react-router-dom"
+import Navbar from '../Navbar'
 
 
 export default function Users(){
     return(
         <>
+        <Navbar/>
+        <div>
+            <h1>Search Users</h1>
+
+
+
+
+
+
+
+
+        </div>
         
         
         </>
